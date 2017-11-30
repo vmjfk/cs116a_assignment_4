ab303303247d65d4f73a301a2159b58199245b3f
vmjfk <vmjfk@kennedylaws.org>
2017-11-29 18:04:18 -0800

    logic in place needs to draw

--- kennedy_john_programming_assignment_4.c
+++ kennedy_john_programming_assignment_4.c
@@ -7,9 +7,86 @@
 #define MAIN_WINDOW_WIDTH 640
 #define MAIN_WINDOW_HEIGHT 480
 
+struct drawSpec {
+    int shapeType; // 0=rect,1=line,2=ellipse,3=bezier
+    int isFilled; // 1 = filled, 0 = not filled
+    int complete; //1=complete
+    int pointsEntered; //number of points that have been entered. shows how close we are to complete. 
+    GLfloat r,g,b;
+    int x[4];
+    int y[4];
+};
+struct drawSpec shapeQueue[100];
+int totalShapes = 0;
 
+void printDrawSpec(struct drawSpec spec)
+{
+    printf("printDrawSpec called.\n");
+    printf("shapeType = %d\n",spec.shapeType);
+    printf("isFilled = %d\n",spec.isFilled);
+    printf("complete = %d\n",spec.complete);
+    printf("points Entered = %d\n",spec.pointsEntered);
+    printf("r,g,b = %f, %f, %f\n",spec.r,spec.g,spec.b);
+    for(int i=0;i<spec.pointsEntered;i++)
+    {
+        printf("point %d: x = %d, y = %d\n",i,spec.x[i],spec.y[i]);
+    }
+    
+}
+
+void drawRect(struct drawSpec shape)
+{
+    printf("drawRect called\n");
+
+    glColor3f (shape.r,shape.g,shape.b);
+    glBegin (GL_POLYGON);
+    glVertex3d (shape.x[0],shape.y[0], 0);
+    glVertex3d (shape.x[1],shape.y[0], 0);
+    glVertex3d (shape.x[1],shape.y[1], 0);
+    glVertex3d (shape.x[0],shape.y[1], 0);
+    glEnd ();
+    glFlush ();
+
+}
+void drawLine(struct drawSpec shape)
+{
+    printf("drawLine called\n");
+
+}void drawEllipse(struct drawSpec shape)
+{
+    printf("drawEllipse called\n");
+
+}void drawBezier(struct drawSpec shape)
+{
+    printf("drawBezier called\n");
+
+}
 void loadShapes()
 {
+    // loops through the shape queue and call a draw program for each one. 
+    // detgermines which draw to do by the "shapetype" attribute of the drawspec. 
+    for(int i=0;i<totalShapes;i++)
+    {
+        if(shapeQueue[i].complete == 1)
+        {
+            switch (shapeQueue[i].shapeType) {
+                case 0:
+                    drawRect(shapeQueue[i]);
+                    break;
+                case 1: 
+                    drawLine(shapeQueue[i]);
+                    break;
+                case 2:
+                    drawEllipse(shapeQueue[i]);
+                    break;
+                case 3:
+                    drawBezier(shapeQueue[i]);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 
 }
 
@@ -22,17 +99,137 @@ void reshow()
 void mouse(int button, int state, int x, int y)
 {
     printf("button = %d, state = %d, x = %d, y = %d\n",button,state,x,y);
+
+    if(button != 2)return;//only works with right mouse button
+    if(state != 0)return; //only on down mouse not up  mouse
+
+    int currentShape = totalShapes;
+    if(shapeQueue[totalShapes].complete == 0) // don't do anything without a spec waiting to be filled
+    {
+        shapeQueue[totalShapes].x[shapeQueue[totalShapes].pointsEntered] = x;
+        shapeQueue[totalShapes].y[shapeQueue[totalShapes].pointsEntered] = y;
+        shapeQueue[totalShapes].pointsEntered++;
+        int pointsNeeded = 0;
+        switch(shapeQueue[totalShapes].shapeType) // 0=rect,1=line,2=ellipse,3=bezier
+        {
+            case 0: //needs two points
+            case 1: //needs two points
+            case 2: //needs two points
+                pointsNeeded = 2;
+                break;
+            case 3: //needs four points
+                pointsNeeded = 4;
+                break;
+            default:
+                break;
+        }
+        if(shapeQueue[totalShapes].pointsEntered == pointsNeeded)
+        {
+            shapeQueue[totalShapes].complete = 1;
+            totalShapes++;
+        }
+    }
+    printDrawSpec(shapeQueue[currentShape]);
+    loadShapes();
+
  
 }
 void mainMenu(int value)
 {  
+    // this gets called after a menu choice
+    // creates a new drawSpec 
+    // sets the "complete" flag to 0;
+    
+    printf("mainMenu called: %d\n",value);
+    
+    switch(value / 10)
+    {
+        case 0: //filled rectangle
+            shapeQueue[totalShapes].shapeType = 0;
+            shapeQueue[totalShapes].isFilled = 1;
+            break;
+        case 1: //unfilled rectangle
+            shapeQueue[totalShapes].shapeType = 0;
+            shapeQueue[totalShapes].isFilled = 0;
+            break;
+        case 2: //filled ellipse
+            shapeQueue[totalShapes].shapeType = 2;
+            shapeQueue[totalShapes].isFilled = 1;
+            break;
+        case 3: //unfilled ellipse
+            shapeQueue[totalShapes].shapeType = 2;
+            shapeQueue[totalShapes].isFilled = 0;
+            break;
+        case 4: //line
+            shapeQueue[totalShapes].shapeType = 1;
+            shapeQueue[totalShapes].isFilled = 0;
+            break;
+        case 5: // bezier
+            shapeQueue[totalShapes].shapeType = 3;
+            shapeQueue[totalShapes].isFilled = 0;
+            break;
+        default:
+            break;
+    }
+    switch(value % 10)
+    {
+        case 0: //red
+            shapeQueue[totalShapes].r = 1.0;
+            shapeQueue[totalShapes].g = 0.0;
+            shapeQueue[totalShapes].b = 0.0;
+            break;
+        case 1: //green
+            shapeQueue[totalShapes].r = 0.0;
+            shapeQueue[totalShapes].g = 1.0;
+            shapeQueue[totalShapes].b = 0.0;
+            break;
+        case 2: //blue 
+            shapeQueue[totalShapes].r = 0.0;
+            shapeQueue[totalShapes].g = 0.0;
+            shapeQueue[totalShapes].b = 1.0;
+            break;
+        case 3: //yellow
+            shapeQueue[totalShapes].r = 1.0;
+            shapeQueue[totalShapes].g = 1.0;
+            shapeQueue[totalShapes].b = 0.0;
+            break;
+        case 4: //purple
+            shapeQueue[totalShapes].r = 1.0;
+            shapeQueue[totalShapes].g = 0.0;
+            shapeQueue[totalShapes].b = 1.0;
+            break;
+        case 5: //orange
+            shapeQueue[totalShapes].r = 1.0;
+            shapeQueue[totalShapes].g = 0.5;
+            shapeQueue[totalShapes].b = 0.0;
+            break;
+        case 6: //white
+            shapeQueue[totalShapes].r = 1.0;
+            shapeQueue[totalShapes].g = 1.0;
+            shapeQueue[totalShapes].b = 1.0;
+            break;
+        case 7: //cyan
+            shapeQueue[totalShapes].r = 0.0;
+            shapeQueue[totalShapes].g = 1.0;
+            shapeQueue[totalShapes].b = 1.0;
+            break;
+    }
+
+    shapeQueue[totalShapes].complete = 0;
+    shapeQueue[totalShapes].pointsEntered = 0;
+    printDrawSpec(shapeQueue[totalShapes]);
+
+
+
 }
 
 void init(void)
 {
 
-    glClearColor(0.0,0.0,0.0,0.0);
- 
+
+    glClearColor(0,0,0,0);
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0,100,0,200);
 
 
 }
@@ -72,7 +269,7 @@ void buildMenus()
     color[4] = "purple";
     color[5] = "orange";
     color[6] = "white";
-    color[7] = "black";
+    color[7] = "cyan";
 
     
     int ctr = 0;
